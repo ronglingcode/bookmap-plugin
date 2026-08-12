b233f03007a9000e68d08ae8a369d93acce4c7a9
Rong Ling <wumnlr@gmail.com>
2026-08-12 06:19:39 -0700

    Revert "bump version"
    This reverts commit ea28ad690717ac731a196568f5231dee5ac4c384.

--- src/main/java/com/bookmap/plugin/rong/PluginVersion.java
+++ src/main/java/com/bookmap/plugin/rong/PluginVersion.java
@@ -2,7 +2,7 @@ package com.bookmap.plugin.rong;
 
 public final class PluginVersion {
 
-    public static final String VERSION = "1.19";
+    public static final String VERSION = "1.17";
 
     private PluginVersion() {}
 }
