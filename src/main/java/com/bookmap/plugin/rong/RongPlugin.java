1ab6e55b56cae35c3e562d7939a7a725031cf516
Rong Ling <wumnlr@gmail.com>
2026-08-12 06:19:39 -0700

    Revert "fix multiple symbols"
    This reverts commit 2abd71e8e4f458663d4df6f1673a485ab481cd07.

--- src/main/java/com/bookmap/plugin/rong/RongPlugin.java
+++ src/main/java/com/bookmap/plugin/rong/RongPlugin.java
@@ -39,7 +39,6 @@ import velox.api.layer1.annotations.Layer1StrategyName;
 import velox.api.layer1.data.InstrumentInfo;
 import velox.api.layer1.data.TradeInfo;
 import velox.api.layer1.messages.Layer1ApiSoundAlertMessage;
-import velox.api.layer1.messages.indicators.AliasFilter;
 import velox.api.layer1.messages.indicators.Layer1ApiUserMessageModifyIndicator.GraphType;
 import velox.api.layer1.messages.indicators.Layer1ApiUserMessageModifyScreenSpacePainter;
 import velox.api.layer1.simplified.Api;
@@ -239,13 +238,11 @@ public class RongPlugin implements CustomModuleAdapter,
         wallChangePainter.registerInstrument(cleanAlias);
         patternSignalPainter.registerInstrument(cleanAlias);
         filledExecutionPainter.registerInstrument(cleanAlias);
-        AliasFilter painterAliasFilter = exactAliasFilter(rawAlias);
 
         // Register ScreenSpacePainter to receive chart coordinate mappings
         api.sendUserMessage(Layer1ApiUserMessageModifyScreenSpacePainter.builder(
                 RongPlugin.class, ChartHoverHotkeyHandler.PAINTER_NAME_PREFIX + cleanAlias)
                 .setScreenSpacePainterFactory(chartHoverHotkeyHandler)
-                .setAliasFilter(painterAliasFilter)
                 .setIsAdd(true)
                 .build());
 
@@ -253,7 +250,6 @@ public class RongPlugin implements CustomModuleAdapter,
         api.sendUserMessage(Layer1ApiUserMessageModifyScreenSpacePainter.builder(
                 RongPlugin.class, PriceZonePainter.PAINTER_NAME_PREFIX + cleanAlias)
                 .setScreenSpacePainterFactory(priceZonePainter)
-                .setAliasFilter(painterAliasFilter)
                 .setIsAdd(true)
                 .build());
 
@@ -261,31 +257,26 @@ public class RongPlugin implements CustomModuleAdapter,
         api.sendUserMessage(Layer1ApiUserMessageModifyScreenSpacePainter.builder(
                 RongPlugin.class, "priceLines_" + cleanAlias)
                 .setScreenSpacePainterFactory(priceLinePainter)
-                .setAliasFilter(painterAliasFilter)
                 .setIsAdd(true)
                 .build());
         api.sendUserMessage(Layer1ApiUserMessageModifyScreenSpacePainter.builder(
                 RongPlugin.class, OrderWallLabelPainter.PAINTER_NAME_PREFIX + cleanAlias)
                 .setScreenSpacePainterFactory(wallLabelPainter)
-                .setAliasFilter(painterAliasFilter)
                 .setIsAdd(true)
                 .build());
         api.sendUserMessage(Layer1ApiUserMessageModifyScreenSpacePainter.builder(
                 RongPlugin.class, OrderWallChangePainter.PAINTER_NAME_PREFIX + cleanAlias)
                 .setScreenSpacePainterFactory(wallChangePainter)
-                .setAliasFilter(painterAliasFilter)
                 .setIsAdd(true)
                 .build());
         api.sendUserMessage(Layer1ApiUserMessageModifyScreenSpacePainter.builder(
                 RongPlugin.class, PatternSignalPainter.PAINTER_NAME_PREFIX + cleanAlias)
                 .setScreenSpacePainterFactory(patternSignalPainter)
-                .setAliasFilter(painterAliasFilter)
                 .setIsAdd(true)
                 .build());
         api.sendUserMessage(Layer1ApiUserMessageModifyScreenSpacePainter.builder(
                 RongPlugin.class, FilledExecutionPainter.PAINTER_NAME_PREFIX + cleanAlias)
                 .setScreenSpacePainterFactory(filledExecutionPainter)
-                .setAliasFilter(painterAliasFilter)
                 .setIsAdd(true)
                 .build());
 
@@ -317,15 +308,6 @@ public class RongPlugin implements CustomModuleAdapter,
         }
 
         PluginLog.info("[Rong] Plugin initialized for " + cleanAlias);
-    }
-
-    static AliasFilter exactAliasFilter(String expectedAlias) {
-        return new AliasFilter() {
-            @Override
-            public boolean isDisplayedForAlias(String candidateAlias) {
-                return expectedAlias != null && expectedAlias.equals(candidateAlias);
-            }
-        };
     }
 
     @Override
