540a13b14559534687aed922eedc7b3408f6d207
Rong Ling <lingrong1988@gmail.com>
2026-08-23 20:51:13 -0700

    a

--- src/main/java/com/bookmap/plugin/rong/PluginVersion.java
+++ src/main/java/com/bookmap/plugin/rong/PluginVersion.java
@@ -2,7 +2,7 @@ package com.bookmap.plugin.rong;
 
 public final class PluginVersion {
 
-    public static final String VERSION = "1.21";
+    public static final String VERSION = "1.22";
 
     private PluginVersion() {}
 }
